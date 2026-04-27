0c2b396e8573a65022f51afad841ca6303d632da
Juan David López Tapasco <j.lopez18@utp.edu.co>
2026-04-27 13:04:19 +0200

    update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("c:\Users\lopez\OneDrive - Universidad Pontificia Comillas\Documentos\comillas\digitalizacio_examen\project-f6883221-1297-41bd-9eb-b64340b1aab1.json")
+  credentials = file("/home/pansitobimbo1/clave.json")
 
   project = var.gcp-project
   region  = var.gcp-region
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -2,6 +2,6 @@
 gcp-project      = "project-f6883221-1297-41bd-9eb"
 gcp-region       = "europe-west4"
 gcp-zone         = "europe-west4-a"
-gcp-cluster-name = "juanDavid-td2026"
+gcp-cluster-name = "juandavid-td2026"
 gcp-node-count   = 2
 gcp-node-size    = "e2-small"
